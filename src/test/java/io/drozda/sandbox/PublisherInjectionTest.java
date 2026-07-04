5ab0d2b5543457ae5e9583146a636fabb9ae3724
Ivan Drozda <ivandrozda1990@gmail.com>
2026-07-05 02:05:09 +0300

    Add Kafka listener logging

--- src/test/java/io/drozda/sandbox/PublisherInjectionTest.java
+++ src/test/java/io/drozda/sandbox/PublisherInjectionTest.java
@@ -1,18 +1,27 @@
 package io.drozda.sandbox;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import io.drozda.sandbox.model.TradeEvent;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class PublisherInjectionTest {
 
+    private static final Logger log = LoggerFactory.getLogger(PublisherInjectionTest.class);
+
     @Autowired
     TradeEventPublisher tradeEventPublisher;
+
+    @Autowired
+    TradeEventListener tradeEventListener;
 
     @Test
     public void shouldInjectTradeEventPublisher() {
@@ -20,9 +29,16 @@ class PublisherInjectionTest {
     }
 
     @Test
-    public void shouldSendEvent() {
+    public void shouldSendEvent() throws InterruptedException {
         TradeEvent tradeEvent = new TradeEvent("eventId", "tradeId", "symbol", "type");
         tradeEventPublisher.publishTradeEvent(tradeEvent);
+
+        boolean received = tradeEventListener.latch.await(1, java.util.concurrent.TimeUnit.SECONDS);
+
+        assertTrue(received);
+        assertEquals(tradeEvent, tradeEventListener.tradeEvent);
+        log.info("Test observed received trade event: {}", tradeEventListener.tradeEvent);
+        
     }
 
 }
